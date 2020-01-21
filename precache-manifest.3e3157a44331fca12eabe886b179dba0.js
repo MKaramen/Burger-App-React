fb9ad9f90fb3ebdd5ed131bb75c16542fb1e7005
MKaramen <matis.kara@gmail.com>
2020-01-21 15:51:18 +0100

    Updates

---
+++ precache-manifest.3e3157a44331fca12eabe886b179dba0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5c09c3ee7cbac05235468338d22c94",
+    "url": "/burger-Builder-App-React/index.html"
+  },
+  {
+    "revision": "676a52e8a817f4711257",
+    "url": "/burger-Builder-App-React/static/css/main.07d6bca8.chunk.css"
+  },
+  {
+    "revision": "215fac415d0b0cbfc7f1",
+    "url": "/burger-Builder-App-React/static/js/2.634c747e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burger-Builder-App-React/static/js/2.634c747e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "676a52e8a817f4711257",
+    "url": "/burger-Builder-App-React/static/js/main.c236e668.chunk.js"
+  },
+  {
+    "revision": "7ed5ac3b6dd7b2e67741",
+    "url": "/burger-Builder-App-React/static/js/runtime-main.9602f412.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-Builder-App-React/static/media/burger-logo.b8503d26.png"
+  }
+]);
