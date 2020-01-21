afb4a44ca7e07787946fdd56f68cf36af206ddda
MKaramen <matis.kara@gmail.com>
2020-01-21 14:51:04 +0100

    burger builder done for mobile

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,13 @@ import React from "react";
 import styles from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import SideDrawerToggle from "../SideDrawer/SideDrawerToggle/SideDrawerToggle";
 
 const Toolbar = props => (
   <header className={styles.Toolbar}>
-    <div>Menu</div>
+    <div>
+      <SideDrawerToggle clicked={props.toggle} />
+    </div>
 
     <div className={styles.Logo}>
       <Logo />
